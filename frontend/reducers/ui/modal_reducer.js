ca91d1c0a52b39c5a39f6c082a6103d96fff11a9
ben <bdberm@gmail.com>
2017-10-24 17:05:19 -0400

    replace redux-thunk with own version

--- frontend/reducers/ui/modal_reducer.js
+++ frontend/reducers/ui/modal_reducer.js
@@ -1,13 +1,13 @@
 import {RECEIVE_MODAL, REMOVE_MODAL} from '../../actions/ui_actions';
 
-const _defaultState = {};
+const _defaultState = {component: null};
 
 const ModalReducer = (oldState = _defaultState, action) => {
   switch (action.type) {
     case RECEIVE_MODAL:
       return {component: action.component};
     case REMOVE_MODAL:
-      return {};
+      return _defaultState;
     default:
       return oldState;
   }
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,8 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import thunk from '../middleware/thunk';
 import RootReducer from '../reducers/root_reducer';
+
 
 const configureStore = (preloadedState = {}) => (
   createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
