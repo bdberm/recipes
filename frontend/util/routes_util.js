afd0ae3db254f7f819ecb9e7780ac18f547cb543
Benjamin Berman <bdberm@gmail.com>
2017-10-25 14:22:25 -0400

    add protected routes that redirect to root

---
+++ frontend/util/routes_util.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+
+
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.session.currentUser ? true : false,
+
+  };
+};
+
+const Protected = ({component: Component, path, loggedIn}) => (
+  <Route path={path} render={(props) => (
+    loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/" />
+    )
+  )}/>
+);
+
+export const ProtectedRoute = withRouter(connect(mapStateToProps,null)(Protected));
