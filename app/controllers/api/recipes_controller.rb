f23dd4cb937f8130a0ca2f1b5e399a5fff867eeb
Benjamin Berman <bdberm@gmail.com>
2017-10-25 15:48:59 -0400

    fix class name of recipes controller

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,7 +1,9 @@
-class Api::UsersController < ApplicationController
+class Api::RecipesController < ApplicationController
   def index
+    @Recipes = Recipe.all
   end
 
   def show
+    @Recipe = Recipe.find(params[:id])
   end
 end
