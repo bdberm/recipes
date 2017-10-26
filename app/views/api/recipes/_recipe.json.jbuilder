ce9309b77f2e86eb4b1b681d25902600fe63d943
Benjamin Berman <bdberm@gmail.com>
2017-10-26 12:13:01 -0400

    style progress on index

--- app/views/api/recipes/_recipe.json.jbuilder
+++ app/views/api/recipes/_recipe.json.jbuilder
@@ -1,3 +1,4 @@
 
 json.extract! recipe, :id, :author_id, :title
+json.minuteTime recipe.time_minutes
 json.authorName recipe.author.name
--- frontend/components/recipe_index/recipe_index_item.jsx
+++ frontend/components/recipe_index/recipe_index_item.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const RecipeIndexItem = ({recipe}) => (
+  <Link to={`/recipes/${recipe.id}`}>
   <div className="recipe-index-item">
     <div className="recipe-index-image"></div>
-    <div className="receipe-index-info">
+    <div className="recipe-index-info">
       <h3>{recipe.title}</h3>
       <h4>By {recipe.authorName}</h4>
+      <h5>{recipe.minuteTime} minutes</h5>
     </div>
     <button className="save-from-index"></button>
   </div>
+  </Link>
 );
 
 export default RecipeIndexItem;
