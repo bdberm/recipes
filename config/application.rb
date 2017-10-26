18e3330232ca26fa65933c64fb23b197453fa5ea
Benjamin Berman <bdberm@gmail.com>
2017-10-26 17:33:22 -0400

    try to fix heroku paperclip seeding

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module RecipeApp
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
       :storage => :s3,
+      :s3_region => ENV["s3_region"],
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
