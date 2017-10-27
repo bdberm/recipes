1ca1a935a86b94ab2cbd538cf5f6eb5751edd1a7
Benjamin Berman <bdberm@gmail.com>
2017-10-27 09:26:22 -0400

    added min space between index items and made logo clickable to index

--- frontend/components/global_nav/global_nav.jsx
+++ frontend/components/global_nav/global_nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SessionButtonContainer from './session_button_container';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 const mapStateToProps = (state) => (
   {
@@ -15,7 +16,7 @@ const mapStateToProps = (state) => (
 
 const GlobalNav = ({loggedInClass}) => (
   <nav className="global-nav">
-    <div className = "logo" />
+    <Link to="/" className="logo"></Link>
     <div className = {`search-container${loggedInClass}`}>What Would You Like To Cook?</div>
     <ul className={`top-right-buttons${loggedInClass}`}>
       <button className = {`recipe-box-button${loggedInClass}`}>Recipe Box</button>
