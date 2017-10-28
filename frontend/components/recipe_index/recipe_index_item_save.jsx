8328dc501c38c4647b77f36db8c0cd369aff64e2
Benjamin Berman <bdberm@gmail.com>
2017-10-28 12:00:44 -0400

    set up save from index

--- frontend/components/recipe_index/recipe_index_item_save.jsx
+++ frontend/components/recipe_index/recipe_index_item_save.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
 class RecipeIndexItemSave extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    const {recipeId, recipeSaved, launchLogin,
+      saveRecipe, unsaveRecipe} = this.props;
+
+    if (recipeSaved === "No User") {
+      launchLogin();
+    } else if (recipeSaved === true) {
+      unsaveRecipe(recipeId);
+    } else {
+      saveRecipe(recipeId);
+    }
+  }
 
   render () {
+
     return(
-      <button className={this.props.classStr}></button>
+      <button onClick={this.handleClick}
+        className={this.props.classStr}></button>
     );
   }
 
--- frontend/components/recipe_index/recipe_index_item_save_container.jsx
+++ frontend/components/recipe_index/recipe_index_item_save_container.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import RecipeIndexItemSave from './recipe_index_item_save';
+import {saveRecipe, unsaveRecipe} from '../../actions/recipe_actions';
+import {receiveModal} from '../../actions/ui_actions';
+import LoginFormContainer from '../session_forms/login_form_container';
 
 const mapStateToProps = (state, ownProps) => {
     let classStr;
+    let recipeSaved;
     if (!state.session.currentUser) {
       classStr = "save-from-index";
+      recipeSaved = "No User";
     }
     else if (state.session.currentUser.savedRecipeIds.includes(ownProps.recipeId)) {
       classStr = "save-from-index saved";
+      recipeSaved = true;
     } else {
       classStr = "save-from-index";
+      recipeSaved = false;
     }
-    return {classStr};
+    return {classStr, recipeSaved};
 };
 
-export default connect(mapStateToProps, null)(RecipeIndexItemSave);
+const mapDispatchToProps = (dispatch, ownProps) => (
+  {
+    launchLogin: (() => dispatch(receiveModal(LoginFormContainer))),
+    saveRecipe: ((recipeId) => dispatch(saveRecipe(recipeId))),
+    unsaveRecipe: ((recipeId) => dispatch(unsaveRecipe(recipeId))),
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecipeIndexItemSave);
