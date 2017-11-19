9c45890248d70ededf2f4eb0672df570b7d629f2
Benjamin Berman <bdberm@gmail.com>
2017-11-19 11:13:42 -0500

    remove logger from production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,7 +5,7 @@ import RootReducer from '../reducers/root_reducer';
 
 
 const configureStore = (preloadedState = {}) => (
-  createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+  createStore(RootReducer, preloadedState, applyMiddleware(thunk))
 );
 
 export default configureStore;
