1556f70c589816a62883917f960fc0443cfd6a37
Benjamin Berman <bdberm@gmail.com>
2017-11-19 11:24:39 -0500

    update webpack.config.js to get rid of minified warning on Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,10 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ],
 };
