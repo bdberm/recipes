d1bcb3ef0de76574a010cdc9fa10814d5af76f3f
Benjamin Berman <bdberm@gmail.com>
2017-11-28 18:28:20 -0500

    footer styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import GlobalNav from './global_nav/global_nav';
+import Footer from './footer/footer';
 import Modal from './modal';
 import MainPageIndexContainer from './recipe_index/main_page_index_container';
 import RecipeShowContainer from './recipe_show/recipe_show_container';
 import {ProtectedRoute} from '../util/routes_util';
 import RecipeBox from './recipe_index/save_box_index_container';
 import SearchResult from './recipe_index/search_result_index_container';
+
 
 const App = () => (
   <div>
@@ -20,6 +22,9 @@ const App = () => (
       <ProtectedRoute path="/recipe-box" component={RecipeBox} />
       <ProtectedRoute path="/search/:searchQuery" component={SearchResult} />
     </main>
+    <footer>
+      <Footer />
+    </footer>
   </div>
 );
 
--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,7 +5,12 @@ const Footer = () => {
 
   return (
     <section className="footer">
-      
+      <ul className="footer-content">
+        <li>Built By: </li>
+        <li><a href="http://www.benjamin-berman.com/">Benjamin Berman</a></li>
+        <li><a href="https://github.com/bdberm">GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/ben-d-berman/">LinkedIn</a></li>
+      </ul>
     </section>
   );
 };
